c81708b8145b1d3505a8e50dfe5ba04a4b5b599a
Shantanu Jhaveri <sj06434@usc.edu>
2020-05-12 02:11:46 -0700

    a meme for this project

---
+++ motiongraphics_p5/motiongraphics_v2/sketch.js
@@ -0,0 +1,136 @@
+let adjust = 10;
+let colorA,colorB
+var col = {
+  r:0,
+  b:0,
+  g:0,
+}
+let song
+let myFont
+
+function preload() {
+  myFont = loadFont('ITCGrouch.ttf');
+  soundFormats('mp3')
+  song = loadSound('song.mp3');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight, WEBGL);
+    frameRate(30)
+  noCursor();
+  noStroke();
+  textFont(myFont);
+  // textSize(36);
+  textAlign(CENTER, CENTER);
+}
+
+
+
+
+function draw() {
+  // let time = millis();
+  // // rotateX(time / 1000);
+  // rotateZ(time / 100);
+  col.r= random(0,255)
+  col.g= random(0,255)
+  col.b= random(0,255)
+
+  if (keyIsPressed === true) {
+    if (keyCode === UP_ARROW) {
+      adjust++;
+    } else if (keyCode === DOWN_ARROW) {
+      adjust--;
+    }
+    else if (keyCode === 32){
+        if (song.isPlaying()){
+          song.stop();
+        }
+        else{
+          song.play()
+        }
+    }
+  }
+  if (adjust <= 5) {
+    adjust = 5;
+  }
+  if (adjust >= 50) {
+    adjust = 50;
+  }
+  ortho();
+  background(0);
+
+
+  if (mouseIsPressed) {
+    background(col.r,col.g,col.b)
+  }
+
+  for (let i = 0; i < adjust; i++) {
+    for (let j = 0; j < adjust; j++) {
+      let map_x = map(i, 0, adjust - 1, -width / 2, width / 2);
+      let map_y = map(j, 0, adjust - 1, -height / 2, height / 2);
+      trip(map_x, map_y);
+
+      if (mouseIsPressed) {
+        drawTrigger(map_x,map_y)
+      } else {
+        drawWarning(map_x,map_y)
+      }
+
+    }
+
+  }
+}
+
+function trip(x, y) {
+  // let time = millis();
+  // rotateX(time / 1000);
+  // rotateZ(time / 1234);
+  push();
+
+  translate(x, y);
+  let m2mx = map(mouseX, 0, width, -width / 2, width / 2);
+  let m2my = map(mouseY, 0, height, -height / 2, height / 2);
+
+  let roff = dist(m2mx, m2my, x, y) / -2;
+
+  rotateY(radians(mouseX));
+  rotateX(radians(mouseY));
+
+  let s = (width / adjust / 3.2) + roff;
+
+  fill(255);
+  if(mouseIsPressed) {
+    fill(col.r,col.g,col.b);
+
+  }
+  box(s);
+
+  fill(0);
+  if(mouseIsPressed) {
+    fill(0);
+  }
+  sphere(s * 0.6);
+
+  pop();
+}
+
+function drawTrigger(x,y){
+  push();
+
+  translate(x,y);
+  let mmx = map(mouseX, 0, width, -width / 2, width / 2);
+  let mmy = map(mouseY, 0, height, -height / 2, height / 2);
+  let roff = dist(mmx, mmy, x, y) / -2;
+  rotateY(radians(mouseX));
+  rotateX(radians(mouseY));
+  let s = (width / adjust / 2) + roff;
+  textSize(50)
+  text('TRIP', 1, 1);
+
+  pop();
+}
+
+function drawWarning(x,y){
+  textSize(100)
+  text('WARNING', 1, 1);
+}
--- motiongraphics_p5/p5template/sketch.js
+++ motiongraphics_p5/p5template/sketch.js
@@ -1,14 +1,28 @@
-function setup(){
-  var canvas = createCanvas(windowWidth, windowHeight);
-  canvas.parent("sketch");
-  frameRate(60);
+
+let x = 0,
+    y = 0,
+    e = .05,
+    s = 150;;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  fill(255);
 }
 
+function draw() {
+  background(200, e * 255);
 
-function draw(){
-  background(0,150,255);
+  x = ease(mouseX, x, e);
+  y = ease(mouseY, y, e);
+  fill(255);
+  circle(x, y, s);
+
+  debugMouse();
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function debugMouse() {
+  let msg = "direct:\nx: " + nf(mouseX, 3, 1) + " / y: " + nf(mouseY, 3, 1);
+  msg += "\n\nease:\nx: " + nf(x, 3, 1) + " / y: " + nf(y, 3, 1);
+  fill(200);
+  text(msg, x - s / 3, y - s / 5);
 }
